fc5649286b8d9cace16febe746ab4d188c416a46
Diogo Korok <korokd@protonmail.ch>
2021-07-10 10:56:40 -0300

    Set up pop-up container with overlay

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,46 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState, useEffect } from 'react';
+import styled from 'styled-components'
 import './App.css';
 
+interface OverlayProps {
+  visible: boolean
+}
+
+const Overlay = styled.div`
+  background-color: rgba(0, 0, 0, 0.3);
+  display: ${({ visible } : OverlayProps) => visible ? 'initial' : 'none'};
+  height: 100vh;
+  position: absolute;
+  width: 100vw;
+`
+
+const Container = styled.div`
+  background-color: white;
+  border-radius: 5px;
+  box-shadow: 0 2px 4px 0 rgba(0, 0, 0, 0.2);
+  height: 300px;
+  left: 50%;
+  position: absolute;
+  top: 50%;
+  transform: translate(-50%, -50%);
+  width: 90%;
+`
+
 function App() {
+  const delay = 2000
+  const [isVisible, setVisible] = useState(false)
+
+  useEffect(() => {
+    setTimeout(() => {
+      setVisible(true)
+    }, delay)
+  }, [])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Overlay visible={isVisible}>
+      <Container>
+      </Container>
+    </Overlay>
   );
 }
 
